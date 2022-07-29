63b49d0da0ebe74a992d74c36ef8a1ed20387815
Kobe040102 <34294040+Kobe040102@users.noreply.github.com>
2022-07-28 21:10:58 -0700

    Update 3 calculator

--- java.js
+++ java.js
@@ -1,8 +1,85 @@
-let Totalzero = 0;
+let TotalZero = 0;
 let userinput = "0";
-let previousOperator;
+let previousOperator = null;
+const screen = document.querySelector(".screen");
 
-document.querySelector('.#row2').addEventListener("click", function(event){
-    buttonClick(event.target.value);
-});
-function buttonClick(value)
+document
+  .querySelector(".cal-buttons")
+  .addEventListener("click", function (event) {
+    buttonClick(event.target.innerText);
+  });
+
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+    rerender();
+}
+
+function handleNumber(value) {
+  if (userinput === "0") {
+    userinput = value;
+  } else {
+    userinput += value;
+  }
+  
+}
+
+function handleSymbol(value) {
+  switch (value) {
+    case "C":
+      userinput = "0";
+      TotalZero = 0;
+      previousOperator = null;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(userinput));
+      previousOperator = null;
+      userinput = "" + TotalZero;
+      TotalZero = 0;
+      break;
+    case "←":
+      if (userinput.length === 1) {
+        userinput = "0";
+      } else {
+        userinput = userinput.substring(0, userinput.length - 1);
+      }
+      break;
+    default:
+      handleMath(value);
+      break;
+  }
+}
+
+function handleMath(value) {
+    const intBuffer = parseInt(userinput);
+    if(TotalZero === 0){
+        TotalZero = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = value;
+    userinput = "0";
+}
+
+function flushOperation(intBuffer) {
+    if(previousOperator === "+"){
+        TotalZero += intBuffer;
+    } else if(previousOperator === "-"){
+        TotalZero -= intBuffer;
+    } else if(previousOperator === "×"){
+        TotalZero *= intBuffer;
+    } else {
+        TotalZero /= intBuffer;
+    }
+}
+
+function rerender() {
+  screen.innerText = userinput;
+}
